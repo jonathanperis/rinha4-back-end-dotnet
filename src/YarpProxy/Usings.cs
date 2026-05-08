af7cd9fda1f45d98e02fed42b82ab6c4eb117d76
Jonathan Peris <jperis.silva@gmail.com>
2026-05-08 10:08:41 -0300

    perf: add yarp benchmark lane

---
+++ src/YarpProxy/Usings.cs
@@ -0,0 +1,15 @@
+global using System;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Net;
+global using System.Net.Http;
+global using System.Net.Sockets;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Yarp.ReverseProxy.Forwarder;
